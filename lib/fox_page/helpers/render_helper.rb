2716538b50260104c7b4bee73f3b30c7c53ca0b8
Georg Gadinger <nilsding@nilsding.org>
2019-07-16 21:17:24 +0200

    Allow passing parameters to the `render` helper

--- lib/fox_page/helpers/render_helper.rb
+++ lib/fox_page/helpers/render_helper.rb
@@ -3,14 +3,14 @@
 module FoxPage
   module Helpers
     module RenderHelper
-      def render(view)
+      def render(view, params = {})
         full_path = Dir.glob(app.root.join("app/views/#{view}.*")).first
 
         unless full_path
           raise ArgumentError, "Could not find template for #{view}"
         end
 
-        Tilt.new(full_path).render(self)
+        Tilt.new(full_path).render(self, params)
       end
     end
   end
