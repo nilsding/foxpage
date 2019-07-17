486372c4c452d6cce906971ff467c0ad0badb0fe
Georg Gadinger <nilsding@nilsding.org>
2019-07-17 21:23:13 +0200

    Include svg images in assets

--- lib/fox_page/builders/assets.rb
+++ lib/fox_page/builders/assets.rb
@@ -20,7 +20,7 @@ module FoxPage
 
       def image_assets
         image_assets_path = app.root.join("app/assets/images")
-        Dir.glob("#{image_assets_path}/**/*.{png,jpg,gif,jpeg}")
+        Dir.glob("#{image_assets_path}/**/*.{png,jpg,gif,jpeg,svg}")
            .map { |full_path| full_path.sub(%r{\A#{image_assets_path}/}, "") }
       end
     end
