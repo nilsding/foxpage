aa424f70577e62c55b9c0fa422efcb1e20c415d0
Georg Gadinger <nilsding@nilsding.org>
2019-10-01 22:11:13 +0200

    Allow prepending a path to the assets dir

--- lib/fox_page/helpers/assets_helper.rb
+++ lib/fox_page/helpers/assets_helper.rb
@@ -5,12 +5,13 @@ require "sprockets"
 module FoxPage
   module Helpers
     module AssetsHelper
-      def asset_path(source)
-        File.join("/assets", app.sprockets.manifest.assets[source])
+      def asset_path(source, prepend: "")
+        prepend +
+          File.join("/assets", app.sprockets.manifest.assets[source])
       end
 
-      def stylesheet_link_tag(source)
-        %(<link rel="stylesheet" href=#{asset_path(source).inspect} />)
+      def stylesheet_link_tag(source, prepend: "")
+        %(<link rel="stylesheet" href=#{asset_path(source, prepend: prepend).inspect} />)
       end
     end
   end
